29b173ed2edf61bc02e464e9c345bd0d518684e7
Sergio I. Urbina <checor@gmail.com>
2014-08-21 19:50:54 -0500

    Construyendo la funcion...

--- main.py
+++ main.py
@@ -5,20 +5,34 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def get_amazon_results(keywords):
-	search_url = "http://www.amazon.com/s/field-keywords=" + keywords
-	response = urllib2.urlopen(search_url)
-	html = response.read()
-	soup = BeautifulSoup(html)
-	products = soup.find_all(class_ = "rslt prod celwidget")
-	for elem in products:
-		price = elem.find_all(class_="bld lrg red")
-		print "Precio encontrado: ", price, type(price)
-	return products
-
+    print "Buscando", keywords, ". . ."
+    keywords = urllib2.quote(keywords)
+    search_url = "http://www.amazon.com/s/field-keywords=" + keywords
+    response = urllib2.urlopen(search_url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    products = soup.findAll(class_ = "prod")
+    names = []
+    prices = []
+    for elem in products:
+        if elem.find(class_="srSprite sprPrime") != None:
+            name = elem.find(class_="lrg bold").string
+            del_price = elem.find(class_="grey").string   
+            price = elem.find(class_="bld lrg red").string
+            print name, del_price, price
+            names.append(name)
+            prices.append(price)
+    print ""
+    return products
+    
 def main():
-	get_amazon_results("funko mlp")
-	return 0
+    while True:
+        item = raw_input("Buscar: ")
+        if item == 'q' or item == '':
+            break
+        get_amazon_results(item)
+    return 0
 
 if __name__ == '__main__':
-	main()
+    main()
 
