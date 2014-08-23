b5ce1c141848bfabbbea3e89e59dac91cba1655b
Sergio I. Urbina <checor@gmail.com>
2014-08-23 14:39:33 -0500

    Funcion de dolar banamex terminada

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def get_banamex_dollar():
-	ban_url = "http://www.banamex.com/economia_finanzas/es/divisas_metales/resumen.htm"
-	response = urllib2.urlopen(ban_url)
-	html = response.read()
-	soup = BeautifulSoup(html)
-	
+    ban_url = "http://www.dolar.mx/precio-del-dolar-hoy/"
+    response = urllib2.urlopen(ban_url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    precios = soup.findAll("h2")
+    #Esto puede ser altamente variable, no confiable
+    valores = str(precios[3])
+    precio = valores[42:47]
+    return float(precio)
+    
+def convert_amzn_price(precio_str):
+    init = precio_str.find('$')
+    init += 1
+    precio = float(precio_str[init:])
+    return precio
+    
+def precio_venta_amzn(usd):
+    pass
 
 def get_amazon_results(keywords):
     print "Buscando", keywords, ". . ."
@@ -21,12 +34,15 @@ def get_amazon_results(keywords):
     products = soup.findAll(class_ = "prod")
     names = []
     prices = []
+    mxn = get_banamex_dollar()
+    print "Precio del dolar:", mxn
     for elem in products:
         if elem.find(class_="srSprite sprPrime") != None:
             name = elem.find(class_="lrg bold").string
             del_price = elem.find(class_="grey").string   
             price = elem.find(class_="bld lrg red").string
             print name, del_price, price
+            print "Precio de venta: ", convert_amzn_price(price)*mxn*1.1+100
             names.append(name)
             prices.append(price)
     print ""
